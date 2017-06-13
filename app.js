fda6cd21eea744572f9b4c6952de8cd105c7a1b6
Jessica M <jmatty16@users.noreply.github.com>
2017-06-12 21:51:28 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+function togfunc() {
+	$("#sidebar-wrapper").removeClass("inactive");
+	$("#close-nav").removeClass("inactive");
+	$("#nav-toggle").addClass("inactive");
+};
+function togfunc2() {
+	$("#sidebar-wrapper").addClass("inactive");
+	$("#close-nav").addClass("inactive");
+	$("#nav-toggle").removeClass("inactive");
+};
