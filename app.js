557e2c9147720bec1cfd0c832dd5bdc141f262a8
Jessica M <jmatty16@users.noreply.github.com>
2017-06-13 12:07:04 -0500

    Add Contact and Videos

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+$(document).ready(function(){
+	//Adds email address to footer
+	var username = "contact";
+	var hostname = "codewithjess.com";
+	var linktext = username + "@" + hostname;
+	$("#contact").prepend("<a href='" + "mail" + "to:" + username + "@" + hostname + "'>" + linktext + "</a>");
+});
+
+//opens nav bar | Warning: will not work if inside .ready function
 function togfunc() {
 	$("#sidebar-wrapper").removeClass("inactive");
 	$("#close-nav").removeClass("inactive");
 	$("#nav-toggle").addClass("inactive");
 };
+//closes nav bar when button or elements pressed | Warning: will not work if inside .ready function
 function togfunc2() {
 	$("#sidebar-wrapper").addClass("inactive");
 	$("#close-nav").addClass("inactive");
